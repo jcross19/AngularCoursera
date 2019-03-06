269a6894452173e602bc522b4602849ea49161e2
jcross19 <46979612+jcross19@users.noreply.github.com>
2019-03-06 10:58:14 -0500

    Update app.js

--- Module2/app.js
+++ Module2/app.js
@@ -20,8 +20,7 @@ function ListAlreadyBoughtController(ShoppingListCheckOffService) {
 	var boughtController = this;
 	boughtController.boughtItems = ShoppingListCheckOffService.getBoughtItems;
 	boughtController.checkIfEmpty = function () {
-		if (boughtController.boughtItems.length == 0);
-		return true
+		return (boughtController.boughtItems.length == 0);
 	};
 };
 
